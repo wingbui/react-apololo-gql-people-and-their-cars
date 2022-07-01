5ac5914e5f97fc170069ddee52ed57c818f1457e
Wing Bui <38932460+wingbui@users.noreply.github.com>
2022-07-01 12:45:53 -0700

    fix: AppSelect did not get value props

--- client/src/components/appSelect/AppSelect.js
+++ client/src/components/appSelect/AppSelect.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const AppSelect = ({ label, handleChange, options, name }) => {
+export const AppSelect = ({ label, handleChange, options, name, value }) => {
   return (
     <div className='grid grid-cols-3'>
       <label className='self-center'>{label}</label>
@@ -8,7 +8,9 @@ export const AppSelect = ({ label, handleChange, options, name }) => {
         className='col-span-2 border border-gray-200 border-solid rounded-l px-2 py-1'
         name={name}
         onChange={handleChange}
+        value={value}
       >
+        <option>--Please choose an option--</option>
         {options.map((o) => {
           return (
             <option key={o.value} value={o.value}>
